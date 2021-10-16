df0b7481d7560d83c085abce50ec07329213b75e
Joel Bello <joel8bl@gmail.com>
2021-10-16 01:36:35 -0400

    fixing styles

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,7 @@
 const headerNavLinks = [
+  { href: '/about', title: 'Sobre mí' },
+  { href: '/projects', title: 'Portfolio' },
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
 ]
 
 export default headerNavLinks
