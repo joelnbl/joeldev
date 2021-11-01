8ecf46f2e442b4819ed877b6e298d8399e020933
Joel Bello <joel8bl@gmail.com>
2021-10-31 22:15:47 -0400

    modifying svg logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        primary: '#892CDC',
+        primary: colors.indigo,
         gray: colors.trueGray,
       },
       typography: (theme) => ({
